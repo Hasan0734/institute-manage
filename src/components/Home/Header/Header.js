4ef24a68158d4c7136616c20468ed55b34063f5c
Hasan07 <Jahidhasan2856@gmail.com>
2021-09-02 08:58:40 +0600

    professor section added

--- src/components/Home/Header/Header.js
+++ src/components/Home/Header/Header.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import HeaderTop from '../HeaderTop/HeaderTop';
 import Navigation from '../Navigation/Navigation';
-import Slider from '../Slider/Slider';
 
 const Header = () => {
     return (
         <>
           <HeaderTop></HeaderTop>
             <Navigation></Navigation>
-            <Slider></Slider>
+            
         </>
     );
 };
--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from '../Header/Header';
+import ProfessorArea from '../ProfessorArea/ProfessorArea';
+import Slider from '../Slider/Slider';
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
+            <Slider></Slider>
+            <ProfessorArea></ProfessorArea>
             <h1>This is Home</h1>
             <h1>This is Home</h1>
             <h1>This is Home</h1>
