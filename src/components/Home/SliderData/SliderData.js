79e88a358115dfb7d978262c99c5eedab6205738
Hasan07 <Jahidhasan2856@gmail.com>
2021-09-02 08:06:48 +0600

    carusele contet added

--- src/components/Home/SliderData/SliderData.js
+++ src/components/Home/SliderData/SliderData.js
@@ -6,24 +6,26 @@ const sliderData = [
     {  
          title: "DU Students' Celebrating their Graduation Moment",
         text: "The main purpose of the University was to create new areas of knowledge and disseminate such knowledge nationally as well ...",
-        info1: `${faUsers} Regular Students : 37000 +`,
-        info2: `${faUsers}Affiliate College Students : 1,70,100 +`,
+        info: [{icon: faUsers, text: "Regular Students : 37000 +"},{icon: faUsers, text:  'Affiliate College Students : 1,70,100 +'}],
+        // info1: {icon: faUsers, text: "Regular Students : 37000 +"},
+        // info2: {icon: faUsers, text:  'Affiliate College Students : 1,70,100 +'},
         image: slider1,
     },
     {  
          title: "The Glorious Journey",
         text: "On the first day of July 1921, the University of Dhaka opened its doors to students with Sir P.J. Hartog ...",
-        info1: `${faUniversity} Departments : 83`,
-        info2: `${faUniversity} Institutes: 13`,
-        info3: `${faUniversity} Research Centers: 54`,
+        info: [{icon: faUniversity, text: 'Departments : 83'},{icon: faUniversity, text: 'Institutes: 13'},{icon: faUniversity, text:'Research Centers: 54'}],
+        // info1: {icon: faUniversity, text: 'Departments : 83'},
+        // info2:  {icon: faUniversity, text: 'Institutes: 13'},
+        // info3: {icon: faUniversity, text:'Research Centers: 54'},
         image: slider2
     },
     {
         title: "Welcome to the University of Dhaka",
         text: "On the first day of July 1921 the University of Dhaka opened its doors to students with Sir P.J. Hartog ...",
-        info1: `${faUniversity} Founded : 1921`,
-        info2: `${faUserShield} Faculty Members : 2000+`,
-        
+        info: [{icon: faUniversity,text: 'Founded : 1921'},{icon: faUserShield, text: 'Faculty Members : 2000+'}],
+        // info1: {icon: faUniversity,text: 'Founded : 1921'},
+        // info2: {icon: faUserShield, text: 'Faculty Members : 2000+'},
         image: slider3
     },
 ]
--- src/components/Home/SliderImage/SliderImage.js
+++ src/components/Home/SliderImage/SliderImage.js
@@ -1,8 +1,12 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import SliderData from "../SliderData/SliderData";
 import './SliderImage.css';
+
+
 const SliderImage = () => {
 
+ 
   return (
     <>
       {SliderData.map((slider, index) => (
@@ -10,23 +14,29 @@ const SliderImage = () => {
           className={` ${
             index - 1 === 0
               ? "carousel-item carusel-dark active"
-              : "carousel-item carusel-dark  "
+              : "carousel-item carusel-dark"
           }`}
           style={{ backgroundImage: `url(${slider.image})` }}
         >
-            <div className="container">
+          
+          <div className="container carusel-content d-flex align-items-center">
                 <div className="row">
-                    <div className="col-md-6 text-white">
-                    <h1>{slider.title}</h1>
+                    <div className="col-md-7 text-white">
+                   <div className="pe-5">
+                   <h3>{slider.title}</h3>
                     <p>{slider.text}</p>
+                  
+                       <button className="btn btn-warning">Read more...</button>
+                
+                    
+                   </div>
                     </div>
-                    <div className="col-md-6 text-white">
-                        <h2>{slider.info1}</h2>
-                        <h2>{slider.info1}</h2>
-                        <h2>{slider.info1}</h2>
+                    <div className="col-md-5 text-white">
+                      {slider.info.map(info => <h3><FontAwesomeIcon className="text-warning" icon={info.icon}/> {info.text}</h3>)}
                     </div>
                 </div>
             </div>
+          
         </div>
       ))}
     </>
